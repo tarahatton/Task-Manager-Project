e2f5c9bc370cc9db949d7ce93851330643b805ec
Shontara Hatton <shontarahatton@Interapts-MacBook-Pro-106.local>
2018-03-02 16:42:09 -0500

    finished

--- Task Manager Assessments/main.swift
+++ Task Manager Assessments/main.swift
@@ -28,7 +28,7 @@ class TaskManager {
     var completed = false
     init(name: String) {
         self.name = name
-    
+        
     }
     
 }
@@ -45,26 +45,29 @@ func createNewTask() {
     var tasks = TaskManager(name: name!)
     taskArray.append(tasks)
     
-    
-    
-    print("Please type in a due date.")
-    var userInput = readLine()
-    
-    let currentDate = Date()
-    print(currentDate)
-    let calendar = Calendar.current
-    
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateStyle = .long
-    dateFormatter.dateFormat = "MM/dd/yyyy HH:mm"
-    print(dateFormatter.string(from: currentDate))
-
-    
 }
+
+
+let currentDate = Date()
+let calendar = Calendar.current
+
+let twoWeeksFromNow = calendar.date(byAdding: .day, value: 14, to: currentDate)
+
+print(twoWeeksFromNow!)
+
+let dateFormatter = DateFormatter()
+
+dateFormatter.dateFormat = "MM/dd/yyyy HH:mm"
+
+print( "Your due date is", dateFormatter.string(from: twoWeeksFromNow!))
+
+
+
 
 func bothTasks() {
     for task in taskArray {
         print(task.name)
+        
     }
 }
 
@@ -78,15 +81,12 @@ func completedTasks() {
 }
 
 func markTask() {
-print("Enter the task you want to mark complete")
-    for task in taskArray {
-        print(task.name)
-
-    }
-    var userInput = readLine()
+    print("Enter the task you want to mark complete")
+    var completeTask = readLine()
     for task in 0...taskArray.count - 1 {
-        if taskArray [task].name == userInput {
-            taskArray [task].completed = true
+        if taskArray[task].name == completeTask {
+            taskArray[task].completed = true
+            print("You have marked", taskArray[task].name , "complete")
         }
     }
 }
@@ -94,24 +94,25 @@ print("Enter the task you want to mark complete")
 func deleteTask() {
     print("Do you want to delete a task?")
     var userInput = readLine()
-   
     for task in 0...taskArray.count - 1 {
         if taskArray[task].name == userInput {
             taskArray.remove(at: task)
         }
-    
+        
     }
 }
 
+var userInput = 0
 
 func mainMenu() {
     print("Press 1 to create new task")
-    print("Press 2 to access all tasks")
+    print("Press 2 to access both tasks")
     print("Press 3 to access completed tasks")
     print("Press 4 to mark a task complete")
     print("Press 5 to delete a task")
     print("Press 6 to exit")
-var userInput = Int(readLine()!)
+    userInput = Int(readLine()!)!
+    
     if userInput == 1 {
         createNewTask()
     }
@@ -130,10 +131,33 @@ var userInput = Int(readLine()!)
     else if userInput == 6 {
         exit(0)
     }
-    
 }
 
+
+while userInput != 6 {
 mainMenu()
+}
+
+
+//var myNumber = 0
+//func enterNumber() {
+//    print("please enter a number, enter 10 to exit")
+//     myNumber = Int(readLine()!)!
+//}
+//
+//while myNumber != 10 {
+//    enterNumber()
+//}
+
+
+
+
+
+
+
+
+
+
 
 
 
